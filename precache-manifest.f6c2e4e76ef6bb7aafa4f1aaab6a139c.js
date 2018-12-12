71574bdca5ec1afcf44f1ca7b8520d3fdd9c778e
Desmond <desmondharris912@gmail.com>
2018-12-12 07:34:20 -0600

    Updates

---
+++ precache-manifest.f6c2e4e76ef6bb7aafa4f1aaab6a139c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/chip-todo/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "39efa51e7a9f97d04a92",
+    "url": "/chip-todo/static/js/runtime~main.39efa51e.js"
+  },
+  {
+    "revision": "7fdf683f4028926abaab",
+    "url": "/chip-todo/static/js/main.7fdf683f.chunk.js"
+  },
+  {
+    "revision": "82dafdb578ba27673946",
+    "url": "/chip-todo/static/js/1.82dafdb5.chunk.js"
+  },
+  {
+    "revision": "7fdf683f4028926abaab",
+    "url": "/chip-todo/static/css/main.afd98cdf.chunk.css"
+  },
+  {
+    "revision": "6ba98d495a9e7479a9b469d5eefacae9",
+    "url": "/chip-todo/index.html"
+  }
+];
